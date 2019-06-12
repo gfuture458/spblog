fc180bc7193d13cb82249044d86e6bd1d3ec7d8a
root <maijiesi123456>
2019-06-12 17:19:03 +0800

    添加模板

---
+++ opinfo/site.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @date: 19-6-12 下午3:49
+# @name: site
+# @author：jh
---
+++ opinfo/urls.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# @date: 19-6-12 下午1:37
+# @name: urls
+# @author：jh
