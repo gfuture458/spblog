4d75240ca2a37000c5d64bd3ca8d3d6880bb66ba
guojiahong <710813711@qq.com>
2019-06-24 09:52:33 +0800

    修改路由

--- opinfo/urls.py
+++ opinfo/urls.py
@@ -11,13 +11,12 @@ from xadmin.plugins import xversion
 
 urlpatterns = [
     path('', views.Info.as_view()),
-    path('me/', views.AboutMe.as_view()), # 关于我
-    path('language/', views.LanguageView.as_view()), # 编程语言
-    path('study/', views.StudyView.as_view()), # 学习笔记
-    path('life/', views.LifeView.as_view()), # 日常生活
-    # path('inspiration/', views.InspirationView.as_view()), # 灵光一现
-    path('time/', views.TimeView.as_view()), # 时间轴
-    path('gbook/', views.GbookView.as_view()), # 留言板
+    # path('me/', views.AboutMe.as_view()), # 关于我
+    path('language', views.LanguageView.as_view()), # 编程语言
+    path('study', views.StudyView.as_view()), # 学习笔记
+    path('life', views.LifeView.as_view()), # 日常生活
+    path('time', views.TimeView.as_view()), # 时间轴
+    path('gbook', views.GbookView.as_view()), # 留言板
     path('info/<bid>', views.InfoView.as_view()), # 文章详情
     path('sub_info/<cid>', views.SubInfoView.as_view()), # 二级文章列表详情
 
